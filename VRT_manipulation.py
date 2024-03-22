86565acd79ed33eab263c405f37d389b35448247
zapletalj-cze <147697542+zapletalj-cze@users.noreply.github.com>
2024-03-22 14:14:06 +0100

    Update VRT_manipulation.py

--- VRT_manipulation.py
+++ VRT_manipulation.py
@@ -1,8 +1,17 @@
 from osgeo import gdal
+import geopandas as gpd
+from tools import get_extent_vector, align_extent_to_snap
+import sys
 
-# Replace these with your values
-large_raster_filename = 'large_file.tif'
-subset_extent = (easting_min, northing_min, easting_max, northing_max)
+large_raster_filename = r'D:\157_CanadaFlood_v3\impervious_areas_5m\big_rasters\TEST.tif'
+domain_test = r'D:\157_CanadaFlood_v3\impervious_areas_5m\small_area.gpkg'
+snap_raster = r'\\euprafapp003\FLST01\01_Projects\157_Canada_Flood_v3\01_MD\01_HAZARD\00_GIS\01_SnapRaster\snap_raster_60.tif'
+vrt_test = r'D:\157_CanadaFlood_v3\impervious_areas_5m\big_rasters\test.vrt'
+
+extent = get_extent_vector(domain_test)
+extent_snapped = align_extent_to_snap(extent, snap_raster, 5)
+print(extent_snapped)
+subset_extent = (extent_snapped[0], extent_snapped[2], extent_snapped[1], extent_snapped[3])
 nodata_value = 99
 
 # Open the large raster
@@ -15,12 +24,19 @@ subset_vrt.SetGeoTransform(large_raster.GetGeoTransform())
 
 # Set VRT parameters for the subset
 band = subset_vrt.GetRasterBand(1)
-band.SetMetadata({'source_0': large_raster_filename})  # Set source filename
-band.SetMetadataItem('source_1', str(1), 'Band')  # Set source band
 band.SetMetadataItem('Subset', 'YES')
 subset_vrt.SetMetadataItem('PixelFunctionType', 'complex')
 subset_vrt.SetMetadata({'SimpleSource': '<SourceFilename relativeToVRT="1">%s</SourceFilename>' % large_raster_filename})
 subset_vrt.SetMetadataItem('SourceRegion', "%d,%d,%d,%d" % (subset_extent[0], subset_extent[1], subset_extent[2], subset_extent[3]))
 
-# ... (rest of the code remains the same)
+# Update target GeoTIFF (optional, can be the same as large_raster_filename)
+target_filename = 'updated_large_file.tif'
+
+# Copy VRT data to target GeoTIFF
+driver = gdal.GetDriverByName('GTiff')
+driver.CopyDataset(ds2_vrt, target_filename)
+
+# Close datasets
+ds1 = None
+ds2_vrt = None
 
